746d6fddd8502a80e97e273083c1b29c95dbd6ec
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-09-14 21:48:09 +0100

    Initial commit

---
+++ app/src/main/java/com/huy1234th/peckbroswer/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.huy1234th.peckbroswer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.huy1234th.peckbroswer.ui.theme.PeckBroswerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PeckBroswerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PeckBroswerTheme {
+        Greeting("Android")
+    }
+}
